82b3188afce648efccaa1dd01ca13604ed0a671d
ruchiparmar7 <ruchiparmar065@gmail.com>
2023-10-10 19:31:24 +0530

    first commit

---
+++ que24/ctrl.js
@@ -0,0 +1,38 @@
+const ang = angular.module("myApp", []);
+
+ang.controller("ctrl", function ($scope, $http) {
+  $scope.alltask = [];
+  $scope.gettask = function () {
+    $http.get("/data").then(function (response) {
+      $scope.alltask = response.data;
+    });
+  };
+  $scope.gettask();
+  $scope.isupdatebtn = false;
+  $scope.isaddbtn = true;
+  $scope.addtask = function () {
+    $http.post("/addtask", $scope.newrecord).then(function () {
+      $scope.gettask();
+    });
+  };
+
+  $scope.deletetask = function (id) {
+    $http.delete("/deltask/" + id).then(function () {
+      $scope.gettask();
+    });
+  };
+
+  $scope.edittask = function (id) {
+    $http.put("/edit/" + id).then(function (res) {
+      $scope.isupdatebtn = true;
+      $scope.isaddbtn = false;
+      $scope.newrecord = res.data;
+    });
+  };
+
+  $scope.updatetask = function (id) {
+    $http.put("/update",$scope.newrecord).then(function () {
+      $scope.gettask();
+    });
+  };
+});
